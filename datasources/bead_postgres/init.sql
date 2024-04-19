4aeea97c72eb29a52f6ddd0a4ce61e15d9a921f2
terenceong95 <terenceong95@gmail.com>
2024-04-19 13:31:53 +0800

    upsert db

--- datasources/bead_postgres/init.sql
+++ datasources/bead_postgres/init.sql
@@ -1,13 +1,12 @@
 CREATE TABLE public.taxi_availability (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    id uuid DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     batch_id uuid NOT NULL,
     location geometry(Point, 4326)
 );
 
 CREATE TABLE public.districts (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    name text NOT NULL PRIMARY KEY,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
-    name text NOT NULL,
     location geometry(MultiPolygon, 4326)
 )
