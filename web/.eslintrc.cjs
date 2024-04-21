eeaba88e10f33a311bded0709209865f24861aca
Dois <kohehd@gmail.com>
2024-04-21 11:44:10 +0800

    chore: added _ rule for unused vars

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -27,5 +27,11 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
+	],
+	rules: {
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+		]
+	}
 };
