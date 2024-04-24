f19bd47ddb9c2b9b1585b3186a39267183ebda18
terenceong95 <terenceong95@gmail.com>
2024-04-24 13:49:44 +0800

    Seperate get_district_taxi_availability into another file

--- spark/main.py
+++ spark/main.py
@@ -1,87 +1,17 @@
-import os
-from pyspark.sql import SparkSession
 from flask import Flask
-
-USER = os.environ['USER']
-PASSWORD = os.environ['PASSWORD']
-DATABASE_HOST = os.environ['DATABASE_HOST']
-
-print("USER: " + USER)
-print("PASSWORD: " + PASSWORD)
-print("DATABASE_HOST: " + DATABASE_HOST)
-
-# (Assuming environment variables are set for connection details)
-url = f"jdbc:postgresql://{DATABASE_HOST}:5432/bead"
-
+from district_taxi_availability import get_district_taxi_availability
 app = Flask(__name__)
-
-# Create SparkSession
-spark = SparkSession.builder.appName("Spark-PostgreSQL") \
-    .config("spark.jars", './postgresql-42.7.3.jar') \
-    .getOrCreate()
 
 @app.route("/district-taxi-availability")
 def read_taxi_availability():
-  """
-  Establishes a SparkSession, reads data from the taxi_availability table,
-  and displays the results. Handles potential errors during connection or data access.
+    """
+    Read data from the taxi_availability table and display latest taxi availability for each district
 
-  Returns:
-    JSON: The result of the SQL query as a JSON object
-  """
+    Returns:
+        JSON: The result of the SQL query as a JSON object
+    """
 
-  # SQL query
-  # Fetches the latest taxi availability data for each district
-  # by joining the districts table with the taxi_availability table
-  # and filtering the latest batch_id
-  # The result is grouped by district and ordered by taxi_count in descending order
-  # The taxi_count is calculated by counting the number of taxis in each district
-  # whose location is within the district's geometry boundary
-  # (i.e., the taxi is within the boundaries of the district)
-  # The distance threshold is set to 0 for exact location matching
-  # (i.e., the taxi's location must be exactly within the district's geometry boundary)
-  sql_query =  """
-    SELECT d.name AS district,
-          COUNT(*) AS taxi_count
-    FROM districts d
-    INNER JOIN (
-        SELECT location,
-              batch_id
-        FROM taxi_availability
-        WHERE batch_id = (
-            SELECT batch_id
-            FROM taxi_availability
-            ORDER BY created_at DESC
-            LIMIT 1
-        )
-    ) AS latest_taxis ON ST_Distance(d.location, latest_taxis.location) <= 0
-    GROUP BY d.name
-    ORDER BY taxi_count DESC
-  """
-
-  try:
-    # Fetch latest taxi availability data for each district
-    print("Fetching taxi availability data...")
-    results_df = spark.read.format("jdbc") \
-      .option("url", url) \
-      .option("driver", "org.postgresql.Driver") \
-      .option("user", USER) \
-      .option("password", PASSWORD) \
-      .option("query", sql_query) \
-      .load()
-    print("Taxi availability data fetched successfully.")
-    
-    # Display the result
-    print("Displaying result...")
-    results_df.show()
-    return results_df.toJSON().collect()
-
-  except Exception as e:
-    print(f"Error reading districts data: {str(e)}")
-
-  finally:
-    # Note: The SparkSession is not stopped here to allow for multiple requests
-    pass
+    return get_district_taxi_availability()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
